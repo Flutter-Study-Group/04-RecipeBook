af9077ade73eb5c0cbb2c539cee4c402f32d37d9
MC <mc@flutcon.net>
2020-08-23 22:17:37 +0200

    add recipe page

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:recipebook/pages/friend_profile.dart';
+import 'package:recipebook/pages/recipe.dart';
 
 void main() {
   runApp(MyApp());
@@ -9,7 +9,7 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
-      home: FriendProfilePage(),
+      home: RecipePage(),
     );
   }
 }
